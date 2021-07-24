5e67d368c104557c0c378333ec3856acac22699b
Vincent <jouanne.vincent@gmail.com>
2021-07-24 10:48:51 +0200

    feat(widget): add weather icon data

---
+++ src/test/widgets/weather_icon_data_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:simple_list_app/widgets/weather_icon_data.dart';
+
+void main() {
+  testWidgets('Weather icon data should correctly show', (tester) async {
+    // Given a weather card
+    MaterialApp app = MaterialApp(
+      home: Scaffold(
+        body: WeatherIconData(
+          icon: Icons.ac_unit,
+          data: "data",
+        ),
+      ),
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+    );
+
+    // When we render the widget
+    await tester.pumpWidget(app);
+
+    //Then the weather card should show up
+    await expectLater(find.byType(WeatherIconData),
+        matchesGoldenFile('goldens/weather_icon_data.png'));
+  });
+}
